89f6e334e5da2475526d8fa97a5845e5b071b351
arimakey <121048453+arimakey@users.noreply.github.com>
2025-03-15 18:53:50 -0500

    feat: inicializa la librería arko-js con configuración básica

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.js"),
+      name: "arko-js",
+      fileName: "arko-js",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+});
