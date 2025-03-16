2b37a2d09fa97da5b962a1f94ddee52c200c4387
arimakey <121048453+arimakey@users.noreply.github.com>
2025-03-16 12:22:20 -0500

    fix: update package.json to use peerDependencies and enhance Vite config for library build formats

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
-import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     build: {
         lib: {
-            entry: resolve('src/index.js'), // Corrección aquí
+            entry: 'src/index.js',
             name: 'arko-js',
-            fileName: 'arko-js',
+            formats: ['es', 'umd'],
+            fileName: (format) => {
+                if (format === 'es') return 'arko-js.js'; 
+                if (format === 'umd') return 'arko-js.umd.cjs';
+            },
         },
         rollupOptions: {
             external: ['react', 'react-dom'],
