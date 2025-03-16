bd65233bf6a34ba681bfc4c73346b3c1b7f5855f
arima_key <127151745+arimakey@users.noreply.github.com>
2025-03-15 22:46:11 -0500

    feat: add Vitest testing script and update Vite configuration for React support

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,23 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(new URL("src/index.js", import.meta.url).pathname),
-      name: "arko-js",
-      fileName: "arko-js",
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+    build: {
+        lib: {
+            entry: resolve('src/index.js'), // Corrección aquí
+            name: 'arko-js',
+            fileName: 'arko-js',
         },
-      },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
     },
-  },
+    plugins: [react()],
 });
